7239b6bb7655d4d31bd1871e0786b5b5b04dd27e
Ståle <stale.jacobsen@student.uib.no>
2021-12-04 14:28:18 +0100

    dirty hack, have a better idea ?

--- 2021/day3.py
+++ 2021/day3.py
@@ -23,5 +23,43 @@ def part_one(array: list):
     return int(gamma, 2) * int(epsilon, 2)
 
 
+def find_one_or_zero(array: list, index: int):
+    zero = 0
+    one = 0
+    for y in range(len(array)):
+        if array[y][index] == '1':
+            one += 1
+        else:
+            zero += 1
+    return 1 if one >= zero else 0
+
+
+def oxygen_generator_rating(array: list):
+    index = 0
+    while len(array) > 1:
+        one_or_zero = find_one_or_zero(array, index)
+        if one_or_zero == 1:
+            array = [row for row in array if row[index] == '1']
+        else:
+            array = [row for row in array if row[index] == '0']
+        index += 1
+
+    return "".join(array[0])
+
+
+def co2_scrubber_rating(array: list):
+    index = 0
+    while len(array) > 1:
+        one_or_zero = find_one_or_zero(array, index)
+        if one_or_zero == 1:
+            array = [row for row in array if row[index] == '0']
+        else:
+            array = [row for row in array if row[index] == '1']
+        index += 1
+
+    return "".join(array[0])
+
+
 if __name__ == '__main__':
     print(f"First star = {part_one(read_input())}")
+    print(f"Second Star = {int(oxygen_generator_rating(read_input()),2) * int(co2_scrubber_rating(read_input()),2)}")
