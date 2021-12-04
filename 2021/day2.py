50aa394932b9bbbba96ac1a42ad982f59c6eb2e3
Ståle <stale.jacobsen@student.uib.no>
2021-12-04 10:22:31 +0100

    forgot to save that file

--- 2021/day2.py
+++ 2021/day2.py
@@ -22,5 +22,25 @@ def dive_day2(array: list) -> int:
     return horizontal_position * depth
 
 
+def part_two(array: list) -> int:
+    horizontal = 0
+    aim = 0
+    depth = 0
+
+    for index, item in enumerate(array):
+        direction = item[0]
+        number = int(item[1])
+        if direction == 'forward':
+            if aim != 0:
+                depth += number * aim
+            horizontal += number
+        elif direction == 'down':
+            aim += number
+        else:
+            aim -= number
+    return horizontal * depth
+
+
 if __name__ == '__main__':
-    print(f"First star: {dive_day2(read_input())}")
+    print(f"First star = {dive_day2(read_input())}")
+    print(f"Second star = {part_two(read_input())}")
